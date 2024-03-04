e9b67047b0c34db2b74ff9165015a8eaa6d11d13
Kartik Arora <kartikcsengineer@gmail.com>
2024-03-04 22:32:00 +0530

    Push auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app";
 
 const start = async () => {
 	console.log("Starting Auth Service...");
+
 	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
 	if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 
