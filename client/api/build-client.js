d225504e0f4ee09c9f59c50e5936ed627ddce144
Kartik Arora <kartikcsengineer@gmail.com>
2024-03-04 23:19:55 +0530

    Updated client

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const buildClient = ({ req }) => {
 	if (typeof window === "undefined") {
 		return axios.create({
-			baseURL: "http://kartik-arora.com",
+			baseURL: "http://kartik-arora.com/",
 			headers: req.headers,
 		});
 	} else {
