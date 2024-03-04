17b5c99d5ba8e7b067c2367dfc0aa1977cd92231
Kartik Arora <kartikcsengineer@gmail.com>
2024-03-04 22:40:12 +0530

    Add depoyment workflows

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,8 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const start = async () => {
+	console.log("Starting Payments Service...");
+
 	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
 	if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
 	if (!process.env.NATS_CLIENT_ID) throw new Error("NATS_CLIENT_ID must be defined");
